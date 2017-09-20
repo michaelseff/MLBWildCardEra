6a02b45086b2165c25091d885c6a366d86315544
draftamerica <steelax42@gmail.com>
2017-09-20 15:01:45 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,232 @@
+$('.button2016').click(function() {
+    $('.bracketarea').css("visibility", "visible");
+});
+
+$('.giantslogo1').hover(function() {
+    $('.giantslogo1').css("opacity", "1");
+    $('.boxes').css("opacity", "0.1");
+    $('.giantslogo2').css("opacity", "0.1");
+    $('.redsoxlogo').css("opacity", "0.1");
+    $('.royalslogo').css("opacity", "0.1");
+    $('.cubslogo').css("opacity", "0.1")
+});
+
+$('.giantslogo1').mouseleave(function() {
+    $('.boxes').css("opacity", "0.8");
+    $('.giantslogo1').css("opacity", "0.8");
+    $('.giantslogo2').css("opacity", "0.8");
+    $('.redsoxlogo').css("opacity", "0.8");
+    $('.royalslogo').css("opacity", "0.8");
+    $('.cubslogo').css("opacity", "0.8")
+});
+
+$('.giantslogo2').hover(function() {
+    $('.giantslogo2').css("opacity", "1");
+    $('.boxes').css("opacity", "0.1");
+    $('.giantslogo1').css("opacity", "0.1");
+    $('.redsoxlogo').css("opacity", "0.1");
+    $('.royalslogo').css("opacity", "0.1");
+    $('.cubslogo').css("opacity", "0.1")
+});
+
+$('.giantslogo2').mouseleave(function() {
+    $('.boxes').css("opacity", "0.8");
+    $('.giantslogo1').css("opacity", "0.8");
+    $('.giantslogo2').css("opacity", "0.8");
+    $('.redsoxlogo').css("opacity", "0.8");
+    $('.royalslogo').css("opacity", "0.8");
+    $('.cubslogo').css("opacity", "0.8")
+});
+
+$('.redsoxlogo').hover(function() {
+    $('.redsoxlogo').css("opacity", "1");
+    $('.boxes').css("opacity", "0.1");
+    $('.giantslogo1').css("opacity", "0.1");
+    $('.giantslogo2').css("opacity", "0.1");
+    $('.royalslogo').css("opacity", "0.1");
+    $('.cubslogo').css("opacity", "0.1")
+});
+
+$('.redsoxlogo').mouseleave(function() {
+    $('.boxes').css("opacity", "0.8");
+    $('.redsoxlogo').css("opacity", "0.8");
+    $('.giantslogo1').css("opacity", "0.8");
+    $('.giantslogo2').css("opacity", "0.8");
+    $('.redsoxlogo').css("opacity", "0.8");
+    $('.royalslogo').css("opacity", "0.8");
+    $('.cubslogo').css("opacity", "0.8")
+});
+
+$('.royalslogo').hover(function() {
+    $('.royalslogo').css("opacity", "1");
+    $('.boxes').css("opacity", "0.1");
+    $('.giantslogo1').css("opacity", "0.1");
+    $('.giantslogo2').css("opacity", "0.1");
+    $('.redsoxlogo').css("opacity", "0.1");
+    $('.cubslogo').css("opacity", "0.1")
+});
+
+$('.royalslogo').mouseleave(function() {
+    $('.boxes').css("opacity", "0.8");
+    $('.royalslogo').css("opacity", "0.8");
+    $('.giantslogo1').css("opacity", "0.8");
+    $('.giantslogo2').css("opacity", "0.8");
+    $('.redsoxlogo').css("opacity", "0.8");
+    $('.royalslogo').css("opacity", "0.8");
+    $('.cubslogo').css("opacity", "0.8")
+});
+
+$('.cubslogo').hover(function() {
+    $('.cubslogo').css("opacity", "1");
+    $('.boxes').css("opacity", "0.1");
+    $('.giantslogo1').css("opacity", "0.1");
+    $('.giantslogo2').css("opacity", "0.1");
+    $('.redsoxlogo').css("opacity", "0.1");
+    $('.royalslogo').css("opacity", "0.1")
+});
+
+$('.cubslogo').mouseleave(function() {
+    $('.boxes').css("opacity", "0.8");
+    $('.cubslogo').css("opacity", "0.8");
+    $('.giantslogo1').css("opacity", "0.8");
+    $('.giantslogo2').css("opacity", "0.8");
+    $('.redsoxlogo').css("opacity", "0.8");
+    $('.royalslogo').css("opacity", "0.8");
+    $('.cubslogo').css("opacity", "0.8")
+});
+
+
+// $('#scorefinder').hover(function(){
+//   $('.scorefindertext').css("visibility", "visible");
+//   $('#scorefinder').css("opacity", "0.2");
+//   $('#scorefinder').css("background-color", "gray")
+// });
+// $("#scorefinder").mouseleave(function(){
+//   $('.scorefindertext').css("visibility", "hidden");
+//   $('#scorefinder').css("opacity", "1")
+// });
+//
+// $('.scorefindertext').hover(function(){
+//   $('.scorefindertext').css("visibility", "visible");
+//   $('#scorefinder').css("opacity", "0.2");
+//   $('#scorefinder').css("background-color", "gray")
+// });
+// $(".scorefindertext").mouseleave(function(){
+//   $('.scorefindertext').css("visibility", "hidden");
+//   $('#scorefinder').css("opacity", "1")
+// });
+//
+// $('#wheelpic').hover(function(){
+//   $('.wheeltext').css("visibility", "visible");
+//   $('#wheelpic').css("opacity", "0.2");
+//   $('#wheelpic').css("background-color", "gray")
+// });
+// $("#wheelpic").mouseleave(function(){
+//   $('.wheeltext').css("visibility", "hidden");
+//   $('#wheelpic').css("opacity", "1")
+// });
+//
+// $('.wheeltext').hover(function(){
+//   $('.wheeltext').css("visibility", "visible");
+//   $('#wheelpic').css("opacity", "0.2");
+//   $('#wheelpic').css("background-color", "gray")
+// });
+// $(".wheeltext").mouseleave(function(){
+//   $('.wheeltext').css("visibility", "hidden");
+//   $('#wheelpic').css("opacity", "1")
+// });
+//
+// $('#draftamerica').hover(function(){
+//   $('.bloginfo').css("visibility", "visible");
+//   $('#draftamerica').css("opacity", "0.2");
+//   $('#draftamerica').css("background-color", "gray")
+// });
+//
+// $("#draftamerica").mouseleave(function(){
+//   $('.bloginfo').css("visibility", "hidden");
+//   $('#draftamerica').css("opacity", "1")
+// });
+//
+// $('#scorefinder').hover(function(){
+//   $('#scorefinder').css("border", "3px solid blue");
+//   $('#scorefinder').css("height", "325px");
+//   $('#scorefinder').css("width", "550px")
+// });
+//
+// $('#scorefinder').mouseleave(function(){
+//   $('#scorefinder').css("border", "1px solid black");
+//   $('#scorefinder').css("height", "300px");
+//   $('#scorefinder').css("width", "505px")
+// });
+//
+// $('.scorefindertext').hover(function(){
+//   $('.scorefindertext').css("visibility", "visible");
+//   $('#scorefinder').css("border", "3px solid blue");
+//   $('#scorefinder').css("height", "325px");
+//   $('#scorefinder').css("width", "550px");
+//   $('#scorefinder').css("opacity", "0.2");
+//   $('#scorefinder').css("background-color", "gray")
+// });
+// $(".scorefindertext").mouseleave(function(){
+//   $('.scorefindertext').css("visibility", "hidden");
+//   $('#scorefinder').css("border", "1px solid black");
+//   $('#scorefinder').css("height", "300px");
+//   $('#scorefinder').css("width", "505px");
+//   $('#scorefinder').css("opacity", "1")
+// });
+//
+// $('#wheelpic').hover(function(){
+//   $('#wheelpic').css("border", "3px solid blue");
+//   $('#wheelpic').css("height", "325px");
+//   $('#wheelpic').css("width", "550px")
+// });
+//
+// $('#wheelpic').mouseleave(function(){
+//   $('#wheelpic').css("border", "1px solid black");
+//   $('#wheelpic').css("height", "300px");
+//   $('#wheelpic').css("width", "505px")
+// });
+//
+// $('.wheeltext').hover(function(){
+//   $('.wheeltext').css("visibility", "visible");
+//   $('#wheelpic').css("border", "3px solid blue");
+//   $('#wheelpic').css("height", "325px");
+//   $('#wheelpic').css("width", "550px");
+//   $('#wheelpic').css("opacity", "0.2");
+//   $('#wheelpic').css("background-color", "gray")
+// });
+// $(".wheeltext").mouseleave(function(){
+//   $('.wheeltext').css("visibility", "hidden");
+//   $('#wheelpic').css("border", "1px solid black");
+//   $('#wheelpic').css("height", "300px");
+//   $('#wheelpic').css("width", "505px");
+//   $('#wheelpic').css("opacity", "1")
+// });
+//
+// $('#draftamerica').hover(function(){
+//   $('#draftamerica').css("border", "3px solid blue");
+//   $('#draftamerica').css("height", "325px");
+//   $('#draftamerica').css("width", "550px")
+// });
+//
+// $('#draftamerica').mouseleave(function(){
+//   $('#draftamerica').css("border", "1px solid black");
+//   $('#draftamerica').css("height", "300px");
+//   $('#draftamerica').css("width", "505px")
+// });
+//
+// $('.bloginfo').hover(function(){
+//   $('.bloginfo').css("visibility", "visible");
+//   $('#draftamerica').css("border", "3px solid blue");
+//   $('#draftamerica').css("height", "325px");
+//   $('#draftamerica').css("width", "550px");
+//   $('#draftamerica').css("opacity", "0.2");
+//   $('#draftamerica').css("background-color", "gray")
+// });
+// $(".bloginfo").mouseleave(function(){
+//   $('.bloginfo').css("visibility", "hidden");
+//   $('#draftamerica').css("border", "1px solid black");
+//   $('#draftamerica').css("height", "300px");
+//   $('#draftamerica').css("width", "505px");
+//   $('#draftamerica').css("opacity", "1")
+// });
